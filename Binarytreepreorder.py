3ff85e7f27c5c485b6752eeef3dc7594959fa570
ayishanajiya9745 <najiyaayisha246@gmail.com>
2026-03-16 00:11:10 +0530

    Add files via upload

---
+++ Binarytreepreorder.py
@@ -0,0 +1,37 @@
+# Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+# Example 1:
+
+# Input: root = [1,null,2,3]
+
+# Output: [1,2,3]
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        result = []
+        
+        def inorder(root):
+            if not root:
+                return
+            
+            
+            result.append(root.val)
+            inorder(root.left)
+            inorder(root.right)
+
+        inorder(root)
+        return result
+        
+ 
